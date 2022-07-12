cfbda10d72f8416e84d87e3c077872ee6cf307c8
Shivaji Patne <shivaji.patne@thermofisher.com>
2022-07-12 17:31:47 +0530

    print button project created

---
+++ PrintProcess/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrintProcess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+            /// <summary>  
+            /// This method creates a dynamic FlowDocument. You can add anything to this  
+            /// FlowDocument that you would like to send to the printer  
+            /// </summary>  
+            /// <returns></returns>  
+            private FlowDocument CreateFlowDocument()
+            {
+                // Create a FlowDocument  
+                FlowDocument doc = new FlowDocument();
+                // Create a Section  
+                Section sec = new Section();
+                // Create first Paragraph  
+                Paragraph p1 = new Paragraph();
+                // Create and add a new Bold, Italic and Underline  
+                Bold bld = new Bold();
+                bld.Inlines.Add(new Run("First Paragraph"));
+                Italic italicBld = new Italic();
+                italicBld.Inlines.Add(bld);
+                Underline underlineItalicBld = new Underline();
+                underlineItalicBld.Inlines.Add(italicBld);
+                // Add Bold, Italic, Underline to Paragraph  
+                p1.Inlines.Add(underlineItalicBld);
+                // Add Paragraph to Section  
+                sec.Blocks.Add(p1);
+                // Add Section to FlowDocument  
+                doc.Blocks.Add(sec);
+                return doc;
+            }
+
+            private void PrintSimpleTextButton_Click(object sender, RoutedEventArgs e)
+            {
+            PrintDialog printDlg = new PrintDialog();
+            printDlg.PrintVisual(this, "Window Printing.");
+            }
+        
+    }
+}
